34b4f9bf0784b2938015d93b4c9758e51fa7cd71
Tony Dunbar <dunbar.tony@gmail.com>
2026-06-02 07:16:49 -0500

    Add DroneNav application constants

---
+++ app/config/constants.py
@@ -0,0 +1,30 @@
+"""
+DroneNav System Constants
+"""
+
+# ------------------------------------------------------------------
+# Site Defaults
+# ------------------------------------------------------------------
+
+DEFAULT_MINIMUM_ALTITUDE_FT = 0
+DEFAULT_MAXIMUM_ALTITUDE_FT = 400
+
+DEFAULT_OPERATIONAL_STATUS = "inactive"
+DEFAULT_SURVEY_STATUS = "not_surveyed"
+
+# ------------------------------------------------------------------
+# SRID
+# ------------------------------------------------------------------
+
+DEFAULT_SRID = 4326
+
+# ------------------------------------------------------------------
+# Site Types
+# ------------------------------------------------------------------
+
+SITE_TYPE_SCHOOL = "school"
+SITE_TYPE_PARK = "park"
+SITE_TYPE_COMMERCIAL = "commercial"
+SITE_TYPE_GOVERNMENT = "government"
+SITE_TYPE_PRIVATE = "private"
+
